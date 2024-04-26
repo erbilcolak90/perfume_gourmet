6e4b6bc26bfb98a740c7d4fc60438cdc13f98b01
Erbil Colak <erbilcolak90@gmail.com>
2024-04-26 21:17:53 +0300

    PG-114-getPerfumesByBrandName-method-defined-service-and-test-classes-and-exception-created

--- src/main/java/com/gourmet/perfume/repository/mongodb/PerfumeRepository.java
+++ src/main/java/com/gourmet/perfume/repository/mongodb/PerfumeRepository.java
@@ -3,8 +3,11 @@ package com.gourmet.perfume.repository.mongodb;
 import com.gourmet.perfume.entity.Perfume;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PerfumeRepository extends MongoRepository<Perfume, String> {
     Optional<Perfume> findByName(String name);
+
+    List<Perfume> findByBrandName(String brandName);
 }
