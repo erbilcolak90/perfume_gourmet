f9dedbf5b253160565278692a39e2ba4cae0dd3c
Erbil Colak <erbilcolak90@gmail.com>
2024-04-26 16:47:40 +0300

    PG-110-perfume-entity-defined-and-type-enums-created

---
+++ src/main/java/com/gourmet/perfume/entity/Perfume.kt
@@ -0,0 +1,20 @@
+package com.gourmet.perfume.entity
+
+import com.gourmet.perfume.enums.TypeEnums
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document("perfumes")
+data class Perfume @JvmOverloads constructor(
+
+        @Id
+        var id: String?,
+        var name: String,
+        var brand: String,
+        var year: Int,
+        var categoryIds: List<String>?,
+        var type: TypeEnums,
+        var content: String?,
+        var description: String?,
+        var thumbnailIds: List<String>?
+): BaseEntity()
---
+++ src/main/java/com/gourmet/perfume/enums/TypeEnums.kt
@@ -0,0 +1,11 @@
+package com.gourmet.perfume.enums
+
+enum class TypeEnums {
+
+    EAU_DE_PERFUME,
+    EAU_DE_TOILETTE,
+    EAU_DE_COLOGNE,
+    EAU_FRAICHE,
+    PURE_PERFUME,
+
+}
