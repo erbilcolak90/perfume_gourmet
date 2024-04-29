74f289e4b861281fc8c6ab0600726dbe5539c712
Erbil Colak <erbilcolak90@gmail.com>
2024-04-29 20:46:34 +0300

    PG-154-getAllUsers-method-defined-service-and-test-classes-input-created

--- src/main/java/com/gourmet/perfume/service/UserService.java
+++ src/main/java/com/gourmet/perfume/service/UserService.java
@@ -1,9 +1,12 @@
 package com.gourmet.perfume.service;
 
+import com.gourmet.perfume.dto.input.user.GetAllUsersInput;
 import com.gourmet.perfume.dto.payload.user.UserPayload;
 import com.gourmet.perfume.entity.User;
 import com.gourmet.perfume.exception.CustomException;
 import com.gourmet.perfume.repository.mongodb.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,5 +20,11 @@ public class UserService {
 
     public UserPayload getUserById(String id){
         return UserPayload.convert(userRepository.findById(id).orElseThrow(()->CustomException.userNotFound(id)));
+    }
+
+    public Page<User> getAllUsers(GetAllUsersInput getAllUsersInput){
+        Pageable pageable = getAllUsersInput.toPageable();
+
+        return userRepository.findAll(pageable);
     }
 }
