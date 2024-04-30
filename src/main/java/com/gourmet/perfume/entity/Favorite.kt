3604e7e74cea1380547d6de580cc0d0d8439851f
Erbil Colak <erbilcolak90@gmail.com>
2024-04-30 13:00:58 +0300

    PG-171-favorite-entity-defined

---
+++ src/main/java/com/gourmet/perfume/entity/Favorite.kt
@@ -0,0 +1,17 @@
+package com.gourmet.perfume.entity
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.elasticsearch.annotations.Document
+import org.springframework.data.elasticsearch.annotations.Field
+import org.springframework.data.elasticsearch.annotations.FieldType
+
+@Document(indexName = "favorites")
+data class Favorite(
+
+        @Id
+        var id: String?,
+        @Field(type = FieldType.Keyword)
+        var userId: String,
+        @Field(type = FieldType.Keyword)
+        var perfumeId: String
+)
