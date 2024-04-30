3f918e8b78934fdd90f66dc9b476592a63c72467
Erbil Colak <erbilcolak90@gmail.com>
2024-04-30 13:14:57 +0300

    PG-173-getFavoriteById-method-defined-service-and-test-classes

--- src/main/java/com/gourmet/perfume/service/FavoriteService.java
+++ src/main/java/com/gourmet/perfume/service/FavoriteService.java
@@ -1,5 +1,9 @@
 package com.gourmet.perfume.service;
 
+import com.gourmet.perfume.dto.payload.user.UserPayload;
+import com.gourmet.perfume.entity.Favorite;
+import com.gourmet.perfume.entity.User;
+import com.gourmet.perfume.exception.CustomException;
 import com.gourmet.perfume.repository.elasticsearch.FavoriteRepository;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +19,9 @@ public class FavoriteService {
         this.favoriteRepository = favoriteRepository;
         this.userService = userService;
         this.perfumeService = perfumeService;
+    }
+
+    public Favorite getFavoriteById(String id){
+        return favoriteRepository.findById(id).orElseThrow(()->CustomException.favoriteNotFound(id));
     }
 }
--- src/test/java/com/gourmet/perfume/FavoriteServiceTest.java
+++ src/test/java/com/gourmet/perfume/FavoriteServiceTest.java
@@ -1,16 +1,24 @@
 package com.gourmet.perfume;
 
 import com.gourmet.perfume.entity.Favorite;
+import com.gourmet.perfume.exception.CustomException;
 import com.gourmet.perfume.repository.elasticsearch.FavoriteRepository;
 import com.gourmet.perfume.service.FavoriteService;
 import com.gourmet.perfume.service.PerfumeService;
 import com.gourmet.perfume.service.UserService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class FavoriteServiceTest {
@@ -31,6 +39,28 @@ class FavoriteServiceTest {
     @BeforeEach
     void setUp(){
 
+        favoriteMock = new Favorite("test_id","test_user_id","test_perfume_id");
+
+    }
+
+    @DisplayName("getFavoriteById should return favorite when given id is exist")
+    @Test
+    void testGetFavoriteById_success(){
+        String id = "test_id";
+
+        when(favoriteRepositoryMock.findById(id)).thenReturn(Optional.ofNullable(favoriteMock));
+
+        assertEquals(id,favoriteServiceMock.getFavoriteById(id).getId());
+    }
+
+    @DisplayName("getFavoriteById should throw custom exception favoriteNotFound when given id does not exist")
+    @Test
+    void testGetFavoriteById_favoriteNotFound(){
+        String id = "test_id";
+
+        when(favoriteRepositoryMock.findById(id)).thenReturn(Optional.empty());
+
+        assertThrows(CustomException.class, ()-> favoriteServiceMock.getFavoriteById(id));
     }
 
     @AfterEach
